75c38d2a7ecb1be7faea5a08d9e991f67779a12d
Kyle Blake <klkblake@gmail.com>
2012-01-17 15:42:20 +1030

    Add support for mouse events.

--- events.go
+++ events.go
@@ -12,15 +12,33 @@ type KeyReleaser interface {
 	KeyRelease(keysym *sdl.Keysym)
 }
 
+type MousePresser interface {
+	MousePress(x, y uint16, button uint8)
+}
+
+type MouseReleaser interface {
+	MouseRelease(x, y uint16, button uint8)
+}
+
+type MouseMover interface {
+	MouseMove(x, y uint16, xrel, yrel int16)
+}
+
 type Events struct {
 	KeyPressers []KeyPresser
 	KeyReleasers []KeyReleaser
+	MousePressers []MousePresser
+	MouseReleasers []MouseReleaser
+	MouseMovers []MouseMover
 }
 
 func NewEvents() *Events {
 	events := new(Events)
 	events.KeyPressers = make([]KeyPresser, 0)
 	events.KeyReleasers = make([]KeyReleaser, 0)
+	events.MousePressers = make([]MousePresser, 0)
+	events.MouseReleasers = make([]MouseReleaser, 0)
+	events.MouseMovers = make([]MouseMover, 0)
 	return events
 }
 
@@ -30,6 +48,18 @@ func (events *Events) AddKeyPresser(keyPresser KeyPresser) {
 
 func (events *Events) AddKeyReleaser(keyReleaser KeyReleaser) {
 	events.KeyReleasers = append(events.KeyReleasers, keyReleaser)
+}
+
+func (events *Events) AddMousePresser(mousePresser MousePresser) {
+	events.MousePressers = append(events.MousePressers, mousePresser)
+}
+
+func (events *Events) AddMouseReleaser(mouseReleaser MouseReleaser) {
+	events.MouseReleasers = append(events.MouseReleasers, mouseReleaser)
+}
+
+func (events *Events) AddMouseMover(mouseMover MouseMover) {
+	events.MouseMovers = append(events.MouseMovers, mouseMover)
 }
 
 func (events *Events) Dispatch(event interface{}) {
@@ -44,6 +74,21 @@ func (events *Events) Dispatch(event interface{}) {
 			for _, keyReleaser := range events.KeyReleasers {
 				keyReleaser.KeyRelease(&e.Keysym)
 			}
+		}
+	case sdl.MouseButtonEvent:
+		switch e.Type {
+		case sdl.MOUSEBUTTONDOWN:
+			for _, mousePresser := range events.MousePressers {
+				mousePresser.MousePress(e.X, e.Y, e.Button)
+			}
+		case sdl.MOUSEBUTTONUP:
+			for _, mouseReleaser := range events.MouseReleasers {
+				mouseReleaser.MouseRelease(e.X, e.Y, e.Button)
+			}
+		}
+	case sdl.MouseMotionEvent:
+		for _, mouseMover := range events.MouseMovers {
+			mouseMover.MouseMove(e.X, e.Y, e.Xrel, e.Yrel)
 		}
 	}
 }
